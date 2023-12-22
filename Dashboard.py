28c98bb46ce350666a088fa987970b4c89a8bdee
Camila <camimq85@gmail.com>
2023-12-22 18:58:07 -0300

    atualiza o documento

--- Dashboard.py
+++ Dashboard.py
@@ -3,22 +3,27 @@ import requests
 import pandas as pd
 import plotly.express as px
 
+# Formata números da aplicação
+def formata_numero(valor, prefixo = ''):
+    for unidade in ['', 'mil']:
+        if valor < 1000:
+            return f'{prefixo} {valor:.2f} {unidade}'
+        valor /= 1000
+    return f'{prefixo} {valor:.2f} milhões'
+
 st.title('DASHBOARD DE VENDAS :shopping_trolley:')
 
 # Acessa os dados de vendas à partir de uma API
 url = 'https://labdados.com/produtos'
 response = requests.get(url)
 
-# Transforma o resultado da requisição em um json
-# Esta parte do código usa o método from_dict() da classe pd.DataFrame da biblioteca pandas para criar um objeto DataFrame a partir de um dicionário.
-
-# O método from_dict() permite construir um DataFrame especificando os dados na forma de um dicionário, onde as chaves representam os nomes das colunas e os valores representam os dados de cada coluna.
-
-# Neste trecho de código específico, o método response.json() está retornando um dicionário, e o método from_dict() é usado para converter esse dicionário em um objeto DataFrame chamado dados.
+# Transforma os dados acessados em um DataFrame
 dados = pd.DataFrame.from_dict(response.json())
 
-# Adicionando métricas na aplicação
-st.metric('Receita total', dados['Preço'].sum())
-st.metric('Quantidade de vendas', dados.shape[0])
+coluna1, coluna2 = st.columns(2)
+with coluna1:
+    st.metric('Receita total', formata_numero(dados['Preço'].sum(), 'R$'))
+with coluna2:
+    st.metric('Quantidade de vendas', formata_numero(dados.shape[0]))
 
 st.dataframe(dados)
