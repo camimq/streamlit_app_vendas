93c6e16ea303d3d8664b15ecceb56f3fc4312db4
Camila <camimq85@gmail.com>
2023-12-22 19:31:24 -0300

    atualiza arquivo python

--- Dashboard.py
+++ Dashboard.py
@@ -20,10 +20,31 @@ response = requests.get(url)
 # Transforma os dados acessados em um DataFrame
 dados = pd.DataFrame.from_dict(response.json())
 
+## Tabelas
+receita_estados = dados.groupby('Local da compra')['Preço'].sum()
+receita_estados = dados.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(receita_estados, left_on = 'Local da compra', right_index = True).sort_values('Preço', ascending=False)
+
+## Gráficos
+fig_mapa_receita = px.scatter_geo(receita_estados,
+                                  lat = 'lat',
+                                  lon = 'lon',
+                                  scope = 'south america',
+                                  size = 'Preço',
+                                  template = 'seaborn',
+                                  hover_name = 'Local da compra',
+                                  hover_data={'lat' : False, 'lon' : False},
+                                  title = 'Receita por estado')
+
+
+## Visualização no streamlit
 coluna1, coluna2 = st.columns(2)
 with coluna1:
     st.metric('Receita total', formata_numero(dados['Preço'].sum(), 'R$'))
+    st.plotly_chart(fig_mapa_receita)
 with coluna2:
     st.metric('Quantidade de vendas', formata_numero(dados.shape[0]))
 
 st.dataframe(dados)
+
+#>
+
