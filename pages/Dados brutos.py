2675bda6c5657d61e47ae995f1131da40d5e613e
Camila <camimq85@gmail.com>
2023-12-26 10:03:32 -0300

    adiciona filtros e exapanders na página

--- pages/Dados brutos.py
+++ pages/Dados brutos.py
@@ -10,4 +10,16 @@ response = requests.get(url)
 dados = pd.DataFrame.from_dict(response.json())
 dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format = '%d/%m/%Y')
 
+with st.expander('Colunas'):
+    colunas = st.multiselect('Selecione as colunas', list(dados.columns), list(dados.columns))
+
+st.sidebar.title('Filtros')
+
+with st.sidebar.expander('Nome do produto'):
+    produtos = st.multiselect('Selecione os produtos', dados['Produto'].unique(), dados['Produto'].unique())
+with st.sidebar.expander('Preço do produto'):
+    preco = st.slider('Selecione o preço', 0, 5000, (0,5000))
+with st.sidebar.expander('Data da compra'):
+    data_compra = st.date_input('Selecione a data', (dados['Data da Compra'].min(), dados['Data da Compra'].max()))
+
 st.dataframe(dados)
